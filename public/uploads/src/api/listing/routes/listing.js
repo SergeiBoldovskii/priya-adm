163c39a432669e8ee6effc7b3b435d88726efce9
SergeiBoldovskii <serjiosmirnov@icloud.com>
2025-09-11 04:33:24 +0700

    Update listing.js

--- public/uploads/src/api/listing/routes/listing.js
+++ public/uploads/src/api/listing/routes/listing.js
@@ -1,3 +1,16 @@
 'use strict';
-const { createCoreRouter } = require('@strapi/strapi').factories;
-module.exports = createCoreRouter('api::listing.listing');
+
+module.exports = {
+  routes: [
+    // list
+    { method: 'GET',    path: '/listings',        handler: 'listing.find',    config: { policies: [] } },
+    // single by id
+    { method: 'GET',    path: '/listings/:id',    handler: 'listing.findOne', config: { policies: [] } },
+    // create
+    { method: 'POST',   path: '/listings',        handler: 'listing.create',  config: { policies: [] } },
+    // update
+    { method: 'PUT',    path: '/listings/:id',    handler: 'listing.update',  config: { policies: [] } },
+    // delete
+    { method: 'DELETE', path: '/listings/:id',    handler: 'listing.delete',  config: { policies: [] } },
+  ],
+};
