f0d623094cdc0e072297aa7cf9f4dc481c806f4a
SergeiBoldovskii <serjiosmirnov@icloud.com>
2025-09-11 00:05:37 +0000

    chore: admin/server config from env

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  auth: { secret: env('ADMIN_JWT_SECRET', 'dev-admin-secret-change-me') },
-  apiToken: { salt: env('API_TOKEN_SALT', 'dev-api-token-salt-change-me') },
-  transfer: { token: { salt: env('TRANSFER_TOKEN_SALT', 'dev-transfer-token-salt-change-me') } },
+  auth: { secret: env('ADMIN_JWT_SECRET') },
+  apiToken: { salt: env('API_TOKEN_SALT') },
+  transfer: { token: { salt: env('TRANSFER_TOKEN_SALT') } },
 });
